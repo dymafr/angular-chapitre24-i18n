9fe865cd8491215008c96ff31cdd74aa6291a248
quentin <quentin@dyma.fr>
2025-01-02 16:31:22 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DatePipe } from '@angular/common';
+import { Component, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [DatePipe],
+  template: `
+    <h1 i18n="homepage main title|description@@123">Hello world</h1>
+    <input type="text" placeholder="search" i18n-placeholder />
+    <h2>{{ getTitle() }}</h2>
+    <h3>{{ today | date : 'long' }}</h3>
+    <!-- <p>{ var, plural - select, cas de figure1 { } cas de figure { }   }</p> -->
+    <p>
+      { apple(), plural, =0 {no apple} =1 { one apple } other {
+      {{ apple() }} apples }}
+    </p>
+    <p>{ gender(), select, male {man} female {woman} }</p>
+  `,
 })
 export class AppComponent {
+  gender = signal('female');
   title = 'i18n';
+  today = new Date();
+  apple = signal(150);
+
+  getTitle() {
+    return $localize`I am a subtitle`;
+  }
 }
